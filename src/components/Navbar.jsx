3df4c7eacf7944ead90bd51ab3c896faf7d5ef13
Mrst12 <66439480+Mrst12@users.noreply.github.com>
2023-08-07 13:41:12 +0100

    add content for the logo onto the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar link</div>;
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-navigation">
+      <div className="font-signature">
+        <h1 className="flex items-center justify-center text-5xl cursor-pointer">
+          Lisa
+        </h1>
+        <p className="text-3xl cursor-pointer">Aspiring To Achieve</p>
+      </div>
+      <div>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
