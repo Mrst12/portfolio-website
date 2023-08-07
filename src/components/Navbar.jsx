9ede4275726d527f2269ee1d5d5b1e353037847e
Mrst12 <66439480+Mrst12@users.noreply.github.com>
2023-08-07 14:14:59 +0100

    add state and onclick function to make the mobile menu work

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-navigation">
       <div className="font-signature">
@@ -22,12 +24,18 @@ const Navbar = () => {
       </ul>
 
       {/*Hamburger*/}
-      <div className="md:hidden z-10">
+      <div onClick={handleClick} className="md:hidden z-10">
         <FaBars />
       </div>
 
       {/*mobile menu */}
-      <ul className="absolute top-0 left-0 w-full h-screen bg-navigation flex flex-col justify-center items-center">
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-navigation flex flex-col justify-center items-center"
+        }
+      >
         <li className="py-6 text-4xl">Home</li>
         <li className="py-6 text-4xl">About</li>
         <li className="py-6 text-4xl">Portfolio</li>
