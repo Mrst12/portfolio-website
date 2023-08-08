b993e1dbb6f87b99b66db0d60891ac341f4f4221
Mrst12 <66439480+Mrst12@users.noreply.github.com>
2023-08-08 12:56:03 +0100

    add hover styling and padding to the navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-navigation">
+    <div className="fixed w-full h-[100px] flex justify-between items-center p-4 bg-navigation">
       <div className="font-signature">
         <h1 className="flex items-center justify-center text-5xl cursor-pointer">
           Lisa
