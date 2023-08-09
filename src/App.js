74f8fe410628056236434cc1f881a3b9dbe4894b
Mrst12 <66439480+Mrst12@users.noreply.github.com>
2023-08-09 13:47:45 +0100

    add content to the homepage, style and add on animation to it

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import About from "./components/About.jsx";
 import Portfolio from "./components/Portfolio.jsx";
 import Experience from "./components/Experience.jsx";
 import Contact from "./components/Contact.jsx";
+import "aos/dist/aos.css";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
-      <About />
+      {/*<About />
       <Portfolio />
       <Experience />
-      <Contact />
+  <Contact />*/}
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import profile from "../assets/profile.jpg";
+import AOS from "aos";
+
+AOS.init({
+  duration: 1000,
+});
 
 const Home = () => {
   return (
     <div className="w-full h-screen bg-gradient-to-b from-cyan-100 to-cyan-500">
-      Home page
+      <div className="max-w-[1500px] mx-auto px-4 flex flex-col items-center justify-center h-full md:flex-row">
+        <div>
+          <img
+            src={profile}
+            alt="profile"
+            className="mt-36 mb-10 mx-auto w-2/3 rounded-full border-2 border-cyan-700 shadow-lg shadow-cyan-700"
+          />
+        </div>
+
+        <div className="flex flex-col items-center justify-center h-full">
+          <h1 className="text-2xl sm:text-4xl font-bold" data-aos="slide-right">
+            Lisa Tinmurth
+          </h1>
+          <h2 className="text-xl sm:text-3xl font-bold" data-aos="slide-left">
+            Full Stack Developer
+          </h2>
+          <p className="max-w-[700px] font-semibold py-4" data-aos="slide-up">
+            I'm a full stack web developer, particularly interested in front end
+            development. Currently trying to perfect React and Tailwindcss.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
