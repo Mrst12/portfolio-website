7e1e0fecae0996bb60f731b3237b817260cf6d78
Mrst12 <66439480+Mrst12@users.noreply.github.com>
2023-08-09 11:08:13 +0100

    add social icons tot he navbar and style them

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <div className="w-full h-screen bg-gradient-to-b from-cyan-100 to-cyan-500">
+      Home page
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -16,11 +18,21 @@ const Navbar = () => {
       {/*menu*/}
 
       <ul className="hidden md:flex">
-        <li>Home</li>
-        <li>About</li>
-        <li>Portfolio</li>
-        <li>Experience</li>
-        <li>Contact</li>
+        <li className="hover:underline-offset-4 hover:underline decoration-black">
+          Home
+        </li>
+        <li className="hover:underline-offset-4 hover:underline decoration-black">
+          About
+        </li>
+        <li className="hover:underline-offset-4 hover:underline decoration-black">
+          Portfolio
+        </li>
+        <li className="hover:underline-offset-4 hover:underline decoration-black">
+          Experience
+        </li>
+        <li className="hover:underline-offset-4 hover:underline decoration-black">
+          Contact
+        </li>
       </ul>
 
       {/*Hamburger*/}
@@ -36,15 +48,69 @@ const Navbar = () => {
             : "absolute top-0 left-0 w-full h-screen bg-navigation flex flex-col justify-center items-center"
         }
       >
-        <li className="py-6 text-4xl">Home</li>
-        <li className="py-6 text-4xl">About</li>
-        <li className="py-6 text-4xl">Portfolio</li>
-        <li className="py-6 text-4xl">Experience</li>
-        <li className="py-6 text-4xl">Contact</li>
+        <li className="py-6 text-4xl hover:underline-offset-4 hover:underline decoration-black">
+          Home
+        </li>
+        <li className="py-6 text-4xl hover:underline-offset-4 hover:underline decoration-black">
+          About
+        </li>
+        <li className="py-6 text-4xl hover:underline-offset-4 hover:underline decoration-black">
+          Portfolio
+        </li>
+        <li className="py-6 text-4xl hover:underline-offset-4 hover:underline decoration-black">
+          Experience
+        </li>
+        <li className="py-6 text-4xl hover:underline-offset-4 hover:underline decoration-black">
+          Contact
+        </li>
       </ul>
 
       {/*social icons*/}
-      <div className="hidden"></div>
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 rounded-t-md text-white border-b border-b-white">
+            <a
+              className="flex justify-between items-center w-full "
+              href="https://www.linkedin.com/in/lisa-tinmurth/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333] text-white border-b border-b-white">
+            <a
+              className="flex justify-between items-center w-full "
+              href="https://github.com/Mrst12"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0] text-white border-b border-b-white">
+            <a
+              className="flex justify-between items-center w-full "
+              href="mailto:LisaTinmurth@hotmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Mail <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69] rounded-b-md text-white">
+            <a
+              className="flex justify-between items-center w-full "
+              href="/resume.pdf"
+              target="_blank"
+              rel="noreferrer"
+              download={true}
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
