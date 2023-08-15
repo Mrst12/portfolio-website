5e0be02e37be2b70bb47be6d1e3eeef90e048ffe
Mrst12 <66439480+Mrst12@users.noreply.github.com>
2023-08-15 13:58:53 +0100

    add styling to the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,8 +7,8 @@ const About = () => {
       className="w-full h-screen bg-gradient-to-b from-cyan-100 to-cyan-500"
     >
       <div className="flex flex-col justify-center items-center w- full h-full">
-        <h1>About Me</h1>
-        <p>
+        <h1 className="text-2xl sm:text-4xl font-bold mb-2">About Me</h1>
+        <p className="max-w-[400px] sm:max-w-[700px] sm:text-xl font-semibold py-4">
           I am a junior full stack developer, I started with my journey to
           coding in 2020 as an escape from homeschooling in the evening during
           the first covid lockdown. I have a thirst for knowlege and so took a
@@ -17,7 +17,6 @@ const About = () => {
           interested in front end development as I love to see the final result.
         </p>
       </div>
-      <div className="grid sm:grid-cols-3"></div>
     </div>
   );
 };
