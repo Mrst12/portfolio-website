6615d8ac11849e85128ae314170a01846bf14a9b
Mrst12 <66439480+Mrst12@users.noreply.github.com>
2023-08-31 10:45:59 +0100

    add styling to the list features to make them stand out

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
       className="w-full h-screen bg-gradient-to-b from-cyan-100 to-cyan-500"
     >
       <div className="flex flex-col justify-center items-center">
-        <h1 className="underline decoration-solid underline-offset-4 text-xl sm:text-4xl font-bold mb-2">
+        <h1 className="underline decoration-solid underline-offset-4 text-xl sm:text-4xl font-bold mb-2 mt-3">
           About Me
         </h1>
         <p className="max-w-[400px] sm:max-w-[700px] sm:text-xl font-semibold py-4">
@@ -22,7 +22,7 @@ const About = () => {
           Technologies I use
         </h1>
         {/*menu*/}
-        <div className="flex flex-col item-center justify-between mx-auto space-y-4 md:flex-row md:space-y-0 md:space-x-12 md:mt-6 md:items-start">
+        <div className="flex flex-col sm:text-lg item-center justify-between mx-auto space-y-4 md:flex-row md:space-y-0 md:space-x-12 md:mt-6 md:items-start">
           <div className="flex flex-col items-center w-full md:items-start">
             <div className="flex flex-col items-center md:items-start md:mt-6">
               <p>HTML</p>
@@ -33,7 +33,7 @@ const About = () => {
           </div>
 
           <div className="flex flex-col items-center w-full md:items-start">
-            <h2 className="underline decoration-solid underline-offset-4">
+            <h2 className="underline decoration-solid underline-offset-4 font-semibold">
               Databases
             </h2>
             <div className="flex flex-col items-center md:items-start">
@@ -43,7 +43,7 @@ const About = () => {
           </div>
 
           <div className="flex flex-col items-center w-full md:items-start">
-            <h2 className="underline decoration-solid underline-offset-4">
+            <h2 className="underline decoration-solid underline-offset-4 font-semibold">
               Frameworks
             </h2>
             <div className="flex flex-col items-center md:items-start">
@@ -56,7 +56,7 @@ const About = () => {
           </div>
 
           <div className="flex flex-col items-center w-full md:items-start">
-            <h2 className="underline decoration-solid underline-offset-4">
+            <h2 className="underline decoration-solid underline-offset-4 font-semibold">
               Version control
             </h2>
             <div className="flex flex-col items-center md:items-start">
