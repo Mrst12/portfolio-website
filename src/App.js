b636e658103f7fd418f943753d0ae60501a7243c
Mrst12 <66439480+Mrst12@users.noreply.github.com>
2023-08-31 11:05:42 +0100

    add in background gradient for the portfolio page

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
       <Navbar />
       <Home />
       <About />
-      {/*<Portfolio />
-      <Experience />
+      <Portfolio />
+      {/*<Experience />
   <Contact />*/}
     </div>
   );
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Portfolio = () => {
-  return <div>Portfolio page</div>;
+  return (
+    <div
+      name="portfolio"
+      className="w-full h-screen bg-gradient-to-b from-cyan-100 to-cyan-500"
+    >
+      Portfolio page
+    </div>
+  );
 };
 
 export default Portfolio;
