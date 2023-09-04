4bd4352d72a4c545b2b6ecec0b31b384dfcb454a
Mrst12 <66439480+Mrst12@users.noreply.github.com>
2023-09-04 10:23:45 +0100

    add in the heading with layout to the portfolio page

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,7 +6,13 @@ const Portfolio = () => {
       name="portfolio"
       className="w-full h-screen bg-gradient-to-b from-cyan-100 to-cyan-500"
     >
-      Portfolio page
+      <div className="flex flex-col justify-center items-center w-full">
+        <div className="">
+          <h1 className="underline decoration-solid underline-offset-4 text-xl sm:text-4xl font-bold mb-2 mt-3">
+            My Projects
+          </h1>
+        </div>
+      </div>
     </div>
   );
 };
