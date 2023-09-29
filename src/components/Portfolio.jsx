dedb52fae388499dbcafcb9009a06fdb09d03d77
Mrst12 <66439480+Mrst12@users.noreply.github.com>
2023-09-29 11:01:27 +0100

    add anchor tags to the buttons and and the urls to the array in the portfolio page

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -11,26 +11,38 @@ const Portfolio = () => {
     {
       id: 1,
       src: sickChildrensTrust,
+      github: "https://github.com/Mrst12/sick-childrens-trust-portfolio",
+      live: "https://mrst12.github.io/sick-childrens-trust-portfolio/",
     },
     {
       id: 2,
       src: quizzical,
+      github: "https://github.com/Mrst12/Quizzical",
+      live: "https://mrst12.github.io/Quizzical/",
     },
     {
       id: 3,
       src: shopApp,
+      github: "https://github.com/Mrst12/shop-app",
+      live: "https://shop-app-ne86.onrender.com/",
     },
     {
       id: 4,
       src: stepParentsUnite,
+      github: "https://github.com/Mrst12/step-parents-unite",
+      live: "https://step-parents-unite.onrender.com/",
     },
     {
       id: 5,
       src: appyFamilies,
+      github: "https://github.com/Mrst12/pp5-frontend-react-appy-families",
+      live: "https://appy-families.onrender.com/",
     },
     {
       id: 6,
       src: misterT,
+      github: "https://github.com/Mrst12/mister-t-upvc",
+      live: "https://mistertsupvc.netlify.app/",
     },
   ];
   return (
@@ -45,12 +57,16 @@ const Portfolio = () => {
           </h1>
         </div>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 mt-4">
-          {portfolios.map(({ id, src }) => (
+          {portfolios.map(({ id, src, github, live }) => (
             <div key={id} className="rounded-lg">
               <img src={src} alt="" className="rounded-md w-full h-52" />
               <div>
-                <button>Demo</button>
-                <button>Code</button>
+                <a href={live} target="_blank" rel="noreferrer">
+                  <button>Demo</button>
+                </a>
+                <a href={github} target="_blank" rel="noreferrer">
+                  <button>Code</button>
+                </a>
               </div>
             </div>
           ))}
