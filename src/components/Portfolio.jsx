47ea64510d50813c844faf71fa591fbfc832bc20
Mrst12 <66439480+Mrst12@users.noreply.github.com>
2023-10-31 13:58:55 +0000

    add structure and styling for the portfolio page of the project

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -13,64 +13,94 @@ const Portfolio = () => {
       src: sickChildrensTrust,
       github: "https://github.com/Mrst12/sick-childrens-trust-portfolio",
       live: "https://mrst12.github.io/sick-childrens-trust-portfolio/",
+      description:
+        "A site for families seeking a place to stay whilst their children are in hospital built using HTML and CSS",
     },
     {
       id: 2,
       src: quizzical,
       github: "https://github.com/Mrst12/Quizzical",
       live: "https://mrst12.github.io/Quizzical/",
+      description: "An online quiz built using HTML, CSS and Javascript",
     },
     {
       id: 3,
       src: shopApp,
       github: "https://github.com/Mrst12/shop-app",
       live: "https://shop-app-ne86.onrender.com/",
+      description:
+        "A command line app built using python, useful for creating a shopping list",
     },
     {
       id: 4,
       src: stepParentsUnite,
       github: "https://github.com/Mrst12/step-parents-unite",
       live: "https://step-parents-unite.onrender.com/",
+      description:
+        "A blog site for step parents can come together for support, built using HTML, CSS, JavaScript and Django",
     },
     {
       id: 5,
       src: appyFamilies,
       github: "https://github.com/Mrst12/pp5-frontend-react-appy-families",
       live: "https://appy-families.onrender.com/",
+      description:
+        "A family planner application, built using  HTML, CSS, JavaScript, Python, Django and postgreSQL",
     },
     {
       id: 6,
       src: misterT,
       github: "https://github.com/Mrst12/mister-t-upvc",
       live: "https://mistertsupvc.netlify.app/",
+      description:
+        "A website for Mister t's UPVC shop built using HTML, and CSS.",
     },
   ];
   return (
     <div
       name="portfolio"
-      className="w-full h-full bg-gradient-to-b from-cyan-100 to-cyan-500"
+      className="w-full h-full bg-gradient-to-b from-cyan-100 to-cyan-500 py-6"
     >
-      <div className="flex flex-col justify-center items-center w-full">
-        <div className="">
-          <h1 className="underline decoration-solid underline-offset-4 text-xl sm:text-4xl font-bold mb-2 mt-3">
+      <div className="justify-center items-center mx-auto w-[80%]">
+        <div className="flex items-center justify-center">
+          <h1 className="underline decoration-solid underline-offset-4 text-xl sm:text-4xl font-bold mb-2">
             My Projects
           </h1>
         </div>
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 mt-4">
-          {portfolios.map(({ id, src, github, live }) => (
-            <div key={id} className="rounded-lg">
-              <img src={src} alt="" className="rounded-md w-full h-52" />
-              <div>
-                <a href={live} target="_blank" rel="noreferrer">
-                  <button>Demo</button>
-                </a>
-                <a href={github} target="_blank" rel="noreferrer">
-                  <button>Code</button>
-                </a>
+
+        {portfolios.map(({ id, src, github, live, description }) => (
+          <>
+            <div
+              key={id}
+              className="rounded-lg p-12 md:px-0 border border-cyan-600 justify-center items-center m-6 bg-cyan-200"
+            >
+              <div className="flex rounded-md h-52 justify-center">
+                <img
+                  src={src}
+                  alt="project pictures"
+                  className="rounded-md border border-cyan-600"
+                />
+              </div>
+              <div className="flex justify-center mt-6">
+                <p>{description}</p>
+              </div>
+
+              <div className=" flex items-center justify-center">
+                <button className="p-4 border border-cyan-600 rounded-md bg-navigation m-4 hover:bg-hoveron">
+                  <a href={live} target="_blank" rel="noreferrer">
+                    Live site
+                  </a>
+                </button>
+
+                <button className="p-4 border border-cyan-600 rounded-md bg-navigation m-4  hover:bg-hoveron">
+                  <a href={github} target="_blank" rel="noreferrer">
+                    Github
+                  </a>
+                </button>
               </div>
             </div>
-          ))}
-        </div>
+          </>
+        ))}
       </div>
     </div>
   );
