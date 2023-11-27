6e909b7353db2fafea5af6823c0e2572615b586b
Mrst12 <66439480+Mrst12@users.noreply.github.com>
2023-11-27 10:57:02 +0000

    add form and styling to the contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,50 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact page</div>;
+  return (
+    <div
+      name="achievements"
+      className="w-full h-full bg-gradient-to-b
+      from-cyan-100 to-cyan-500 py-6"
+    >
+      <div className="flex items-center justify-center">
+        <h1 className="underline decoration-solid underline-offset-4 text-xl sm:text-4xl font-bold mb-2">
+          Contact Me
+        </h1>
+      </div>
+
+      <div className="flex rounded-lg p-12 md:px-0 border border-cyan-600 justify-center items-center m-6 bg-cyan-200 ">
+        <form
+          action="https://getform.io/f/aac39631-dfd3-4fb8-9edb-d45140c25379"
+          method="POST"
+          className="flex flex-col w-full md:w-1/2"
+        >
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            className="p-2 bg-cyan-300 border rounded-md border-cyan-600 text-black focus:outline-none placeholder:text-black mb-4"
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            className="p-2 bg-cyan-300 border rounded-md border-cyan-600 text-black focus:outline-none placeholder:text-black mb-4"
+          />
+          <textarea
+            name="message"
+            rows="10"
+            placeholder="Enter your message"
+            className="p-2 bg-cyan-300 border rounded-md border-cyan-600 text-black focus:outline-none placeholder:text-black"
+          ></textarea>
+
+          <button className="p-4 border border-cyan-600 rounded-md bg-navigation m-4 hover:bg-hoveron">
+            Lets Talk!
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
